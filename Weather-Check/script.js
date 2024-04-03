667b31795d26aa51e4b0f4d9462e6a20bbb4c028
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-04-03 20:05:35 +0530

    up

--- Weather-Check/script.js
+++ Weather-Check/script.js
@@ -1,9 +1,16 @@
-const add = document.getElementById('btn1').value;
-const first = document.getElementById('inp1').value;
-const second = document.getElementById('inp2').value;
+const url="https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin&appid="
+const apiKey="44675b3c1e974db877d611d39af185ac"
+const city = document.querySelector(".search");
 
-const container = document.querySelector('.container')
 
-add.addEventListener('click',function(){
-  console.log(first+second)
-})
+async function checkWeather(){
+  const response= await fetch(url+ `${apiKey}`);
+  var data = await response.json();
+  console.log(data)
+
+  document.querySelector(".city").innerHTML=data.name;
+  document.querySelector(".temp").innerHTML=Math.floor(data.main.temp)+`°C`;
+
+}
+
+checkWeather()
