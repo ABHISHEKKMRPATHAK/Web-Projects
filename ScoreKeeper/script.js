eca32dbf7890bc085fa9b73a7a3edaf23778aa00
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-04-06 18:13:46 +0530

    done

---
+++ ScoreKeeper/script.js
@@ -0,0 +1,46 @@
+let p1Display = document.querySelector("#p1");
+let p2Dispay =document.querySelector("#p2");
+let player1=document.querySelector("#player1");
+let player2=document.querySelector("#player2");
+let resetButton=document.querySelector("#reset");
+let winningScoreSelect= document.querySelector("#playTo")
+
+let p1Score=0;
+let p2Score=0;
+let isGameover=false;
+
+let winningScore =  5;
+
+
+player1.addEventListener("click",()=>{
+    if(!isGameover){
+        p1Score+=1;
+    if(p1Score===winningScore){
+         isGameover=true;
+    }
+    p1Display.innerHTML=p1Score;
+    }})
+
+
+player2.addEventListener("click",()=>{
+    if(!isGameover)   {
+        p2Score+=1;
+        if(p2Score===winningScore){
+           isGameover=true;
+        }
+    } 
+p2Dispay.innerHTML=p2Score
+})
+
+resetButton.addEventListener('click',()=>{
+    isGameover=false;
+    p1Score=0;
+    p2Score=0;
+
+    p1Display.innerHTML=p1Score;
+    p2Dispay.innerHTML=p2Score;
+
+})
+winningScoreSelect.addEventListener('change',()=>{
+    alert(this.value);
+})
