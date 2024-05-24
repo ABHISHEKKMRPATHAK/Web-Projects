18e5a78d4c65c7179a775da15a642cc544e7f401
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-05-24 21:22:01 +0530

    upcd

---
+++ Tools/index.js
@@ -0,0 +1,26 @@
+let name1= "abhishek"
+var name2 = "rohan"
+const name3 = "rishav"
+
+function first(){
+
+    let name4= "abhishek1"
+    var name5 = "rohan2"
+    const name6 = "rishav3"
+
+
+    //console.log(name1);
+    //console.log(name2);
+    //console.log(name3);
+
+
+}
+
+name6 = 'ravi' 
+
+    console.log(name6);
+   // console.log(name5);
+    //console.log(name6);
+
+
+window.onload = first
